f84db551f24ae9e8c97468f058affd14b8053097
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2022-12-08 13:42:47 +0600

    Add files via upload

---
+++ Patterns solution/RightTriangle.c
@@ -0,0 +1,23 @@
+
+//C program to print right triangle star pattern
+
+#include<stdio.h>
+
+int main()
+{
+
+
+    int i,j,n;
+
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
